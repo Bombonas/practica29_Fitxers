d39134fd57ed8af2359405d605ad9f241efdd0f4
pablo <pablomejiast@gmail.com>
2023-02-05 17:19:28 +0100

    commit

--- Ejercicios/CARADURA.py
+++ Ejercicios/CARADURA.py
@@ -3,7 +3,7 @@ def ord_dict(dic):
     for pasada in range(len(list_clau) - 1):
         lista_ordenada = True
         for i in range(len(list_clau) - 1 - pasada):
-            if dic[list_clau[i]]["company"] > dic[list_clau[i + 1]]["company"]:
+            if dic[list_clau[i]]["company"].lower() > dic[list_clau[i + 1]]["company"].lower():
                 lista_ordenada = False
                 aux = list_clau[i]
                 list_clau[i] = list_clau[i + 1]
@@ -12,18 +12,24 @@ def ord_dict(dic):
             break
     return list_clau
 
-def save_in_dictionary(file, dic):
+def save_data(file, dic, list):
+    f = open(file, "w")
+    for id in list:
+        txt = id + " | " + dic[id]["company"] + " | " + dic[id]["direction"] + " | " + dic[id]["city"] + "\n"
+        f.write(txt)
+    f.close()
+def load_data(file, dic):
     f = open(file, "r")
     lines = f.readlines()
+    f.close()
     for line in lines:
         values = line.split(" | ")
         dic_aux = {"company": values[1], "direction": values[2], "city": values[3][:-1]}
         dic.update({values[0]: dic_aux})
 
-
-
 dict_total = {}
-save_in_dictionary("Cara.txt", dict_total)
-save_in_dictionary("Dura.txt", dict_total)
-print(dict_total["BVBEV"])
-print(ord_dict(dict_total))
+load_data("Cara.txt", dict_total)
+load_data("Dura.txt", dict_total)
+list_ord = ord_dict(dict_total)
+save_data("clients.txt", dict_total, list_ord)
+
